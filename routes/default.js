7613fba66254550cdf1495f588eb2f1dc6530a42
jackyrusly <jacky.rusly@CARTIER.moonlay.jakarta>
2017-05-12 13:47:38 +0700

    Add Inventory Summary
    default, v1, inventory-summary-router, index
    test/basic, test/index, test/unauthorized

--- routes/default.js
+++ routes/default.js
@@ -1,6 +1,8 @@
 // INVENTORY
 var inventoryDocumentRouter = require('../src/routers/v1/inventory/inventory-document-router');
+var inventorySummaryRouter = require('../src/routers/v1/inventory/inventory-summary-router');
 
 module.exports = function(server) {
-      inventoryDocumentRouter().applyRoutes(server,                    "/inventory/inventory-documents"); 
+      inventoryDocumentRouter().applyRoutes(server,                    "/inventory/inventory-documents");
+      inventorySummaryRouter().applyRoutes(server,                    "/inventory/inventory-summary"); 
 };
--- routes/v1.js
+++ routes/v1.js
@@ -1,6 +1,8 @@
 // INVENTORY
 var v1InventoryDocumentRouter = require('../src/routers/v1/inventory/inventory-document-router');
+var v1InventorySummaryRouter = require('../src/routers/v1/inventory/inventory-summary-router');
 
 module.exports = function(server) {
-      v1InventoryDocumentRouter().applyRoutes(server,                    "/v1/inventory/inventory-documents"); 
+      v1InventoryDocumentRouter().applyRoutes(server,                    "/v1/inventory/inventory-documents");
+      v1InventorySummaryRouter().applyRoutes(server,                    "/v1/inventory/inventory-summary"); 
 };

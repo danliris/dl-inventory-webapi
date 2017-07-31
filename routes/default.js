678a5a4f73e5ec1b0fe53ee6709c5381bbfb22d8
achensetyawati <achenzetya@gmail.com>
2017-07-31 08:36:48 +0700

    routes for fp retur

--- routes/default.js
+++ routes/default.js
@@ -3,8 +3,16 @@ var inventoryDocumentRouter = require('../src/routers/v1/inventory/inventory-doc
 var inventorySummaryRouter = require('../src/routers/v1/inventory/inventory-summary-router');
 var inventoryMovementRouter = require('../src/routers/v1/inventory/inventory-movement-router');
 
+//RETUR To QC
+var fpReturToQCDocRouter = require('../src/routers/v1/inventory/fp-retur-to-qc-doc-router');
+var productByProductionOrderRouter = require('../src/routers/v1/inventory/product-by-production-order-router');
+
 module.exports = function(server) {
       inventoryDocumentRouter().applyRoutes(server,                    "/inventory/inventory-documents");
       inventorySummaryRouter().applyRoutes(server,                    "/inventory/inventory-summary"); 
       inventoryMovementRouter().applyRoutes(server,                    "/inventory/inventory-movement");
+
+      //RETUR To QC
+      fpReturToQCDocRouter().applyRoutes(server,                       "/inventory/fp-retur-to-qc-docs");
+      productByProductionOrderRouter().applyRoutes(server,             "/inventory/products-by-production-orders");
 };
--- routes/v1.js
+++ routes/v1.js
@@ -3,8 +3,17 @@ var v1InventoryDocumentRouter = require('../src/routers/v1/inventory/inventory-d
 var v1InventorySummaryRouter = require('../src/routers/v1/inventory/inventory-summary-router');
 var v1InventoryMovementRouter = require('../src/routers/v1/inventory/inventory-movement-router');
 
+//RETUR to QC
+var v1fpReturToQCDocRouter = require('../src/routers/v1/inventory/fp-retur-to-qc-doc-router');
+var v1productByProductionOrderRouter = require('../src/routers/v1/inventory/product-by-production-order-router');
+
 module.exports = function(server) {
       v1InventoryDocumentRouter().applyRoutes(server,                    "/v1/inventory/inventory-documents");
       v1InventorySummaryRouter().applyRoutes(server,                    "/v1/inventory/inventory-summary"); 
       v1InventoryMovementRouter().applyRoutes(server,                    "/v1/inventory/inventory-movement");
+
+      //RETUR to QC
+      v1fpReturToQCDocRouter().applyRoutes(server,                         "/v1/inventory/fp-retur-to-qc-docs");
+      v1productByProductionOrderRouter().applyRoutes(server,               "/v1/inventory/products-by-production-orders");
+    
 };
